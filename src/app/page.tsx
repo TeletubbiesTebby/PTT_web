04021c50e20966336fe0d0cc965dbe6a60cab0fe
chanyapakSub <111240690+chanyapakSub@users.noreply.github.com>
2024-11-27 10:01:28 +0700

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,42 @@
 import Image from "next/image";
+import Pump from './public/Pump.png'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="grid grid-cols-[6fr_5fr] w-full min-h-screen bg-black">
+      <section className="relative flex justify-self-center h-full items-center ">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={Pump}
+          className="self-center"
+          alt="Pump"
+          width={700}
+          height={700}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <aside className="grid gap-2">
+          <div className="absolute top-[25%] -left-[1%] bg-[#b8b8b8] h-[44px] w-[190px] rounded-3xl items-center flex text-[16px] font-medium px-2 ">
+            Flow Sensor :
+          </div>
+          <div className="absolute top-[35%] -left-[10%]  bg-[#b8b8b8] h-[44px] w-[190px] rounded-3xl items-center flex text-[16px] font-medium px-2 ">
+            Temperature Sensor :
+          </div>
+          <div className="absolute bg-[#b8b8b8] h-[44px] w-[190px] rounded-3xl items-center flex text-[16px] font-medium px-2 ">
+            Pressure Sensor :
+          </div>
+          <div className="absolute bg-[#b8b8b8] h-[44px] w-[190px] rounded-3xl items-center flex text-[16px] font-medium px-2 ">
+            Power Meter :
+          </div>
+          <div className="absolute bg-[#b8b8b8] h-[44px] w-[190px] rounded-3xl items-center flex text-[16px] font-medium px-2 ">
+            Vibration Sensor :
+          </div>
+          <div className="absolute top-[20%] left-[50%] bg-[#b8b8b8] h-[44px] w-[250px] rounded-3xl items-center flex text-[16px] font-medium px-2 ">
+            Ambient Temperature Sensor :
+          </div>
+        </aside>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section>
+
+      </section>
+    </main>
   );
 }
